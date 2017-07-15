f7bdbf62e424420ae80a6d2ad9bee5526c0da5d2
chenyugui <474593676@qq.com>
2017-07-15 17:28:01 +0800

    完成ApiServiceCreator

---
+++ http/src/main/java/com/taichuan/http/OkHttpClientCreator.java
@@ -0,0 +1,31 @@
+package com.taichuan.http;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by gui on 17-7-15.
+ * 单例模式OkHttpClient
+ */
+public class OkHttpClientCreator {
+    private OkHttpClient okHttpClient;
+    private static final int CONNECT_TIMEOUT_SECONDS = 30;
+
+    private OkHttpClientCreator() {
+        okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(CONNECT_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+                .writeTimeout(CONNECT_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+                .readTimeout(CONNECT_TIMEOUT_SECONDS, TimeUnit.SECONDS)
+                .build();
+    }
+
+    public static OkHttpClient getInstance() {
+        return OkHttpClientCreaterHolder.OK_HTTP_CLIENT_CREATOR.okHttpClient;
+    }
+
+
+    private static final class OkHttpClientCreaterHolder {
+        private static final OkHttpClientCreator OK_HTTP_CLIENT_CREATOR = new OkHttpClientCreator();
+    }
+}
---
+++ http/src/main/java/com/taichuan/http/bean/ResultData.java
@@ -0,0 +1,9 @@
+package com.taichuan.http.bean;
+
+/**
+ * Created by gui on 2017/7/15.
+ */
+public class ResultData<T> {
+    private boolean state;
+    private int code;
+}
